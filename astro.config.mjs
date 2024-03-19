77928227dfb97ca7446c00d4f106a6a9c5a4721f
TommyShao <tomieric@gmail.com>
2024-03-19 16:01:05 +0800

    feat: add comment feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightConfig, { locales } from './config/starlight.config';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 export { locales };
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-starlight-fabric-design.vercel.app',
-  integrations: [
-		starlight(starlightConfig), 
-		tailwind({
-			applyBaseStyles: false,
-		})
-	]
+  site: 'https://astro-starlight-fabric-design.vercel.app',
+  integrations: [starlight(starlightConfig), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
